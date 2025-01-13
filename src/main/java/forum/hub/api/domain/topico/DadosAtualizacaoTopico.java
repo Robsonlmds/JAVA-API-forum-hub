ae8fecb47c434a9f2339d586a1838f54dd166c46
RobsonLMDS <98341530+Robsonlmds@users.noreply.github.com>
2025-01-13 18:29:23 -0300

    first commit

---
+++ src/main/java/forum/hub/api/domain/topico/DadosAtualizacaoTopico.java
@@ -0,0 +1,14 @@
+package hub.forum.api.domain.topico;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosAtualizacaoTopico(
+
+        Long id,
+        @NotBlank
+        String titulo,
+        @NotBlank
+        String mensagem,
+        String autor,
+        String curso) {
+}
---
+++ src/main/java/forum/hub/api/domain/topico/EstadoTopico.java
@@ -0,0 +1,12 @@
+package hub.forum.api.domain.topico;
+
+public enum EstadoTopico {
+    ABERTO,
+    FECHADO,
+    EXCLUIDO,
+    PENDENTE,
+    APROVADO,
+    REJEITADO,
+    RESOLVIDO,
+    ARQUIVADO
+}
